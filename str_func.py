e820705e823aa9e4921df1cc593f0b7d1b9625e5
Almaz Muzafarov <muzafrov.almaz2012@yandex.ru>
2024-02-22 04:41:46 +0400

    add docstring and second function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_func(string):
+    """докстринг про функционал первой функции"""
+    string_upper=string.upper()
+    return string_upper
+def first_upper(string):
+    """Докстринг про функционал второй функции"""
     string_title=string.title()
     return string_title
